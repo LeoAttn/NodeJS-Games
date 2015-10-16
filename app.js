d5109c9318efe77dabcbc2a06743a665fae9fdba
Léo <leo.massonnat@hotmail.fr>
2015-10-16 17:13:57 +0200

    *

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./app/routes/index');
 var users = require('./app/routes/users');
+var batailleNavale = require('./app/routes/batailleNavale');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/batailleNavale', batailleNavale);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NodeJS-Games' });
+    res.render('index', { title: 'NodeJS-Games' });
 });
 
 module.exports = router;
