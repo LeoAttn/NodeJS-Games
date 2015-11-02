410a94d75c77ed0a7181882488e50da6534d8852
Léo <leo.massonnat@ynov.com>
2015-11-03 00:04:47 +0100

    ajout js et img fonctionnelle dans play.jade réindentation correct de tous les fichiers

--- app/routes/play.js
+++ app/routes/play.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('play', { title: 'Bataille Navale - En cours' });
+router.get('/', function (req, res, next) {
+    res.render('play', {title: 'Bataille Navale - En cours'});
 });
 
 module.exports = router;
