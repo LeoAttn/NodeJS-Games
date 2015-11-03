739bb30cffd308ce5a4e506906ea8aa18d432952
Léo <leo.massonnat@hotmail.fr>
2015-11-03 13:23:20 +0100

    ajout couleur sur la cellule sur laquelle le joueur a cliqué

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -2,12 +2,11 @@ var clic = 0;
 
 function testlog() {
     clic++;
-    console.log("TEST");
-    console.log(x);
+    console.log("TEST - " + clic);
 }
 
 function clictab(x, y) {
     console.log("Le joueur a clique sur la case x=" + x + " et y=" + y + ".");
-    $(".cellule").css("background", "none");
-    $("."+x+"-"+y).css("background", "red");
+    $(".cell").removeClass("cell-click");
+    $("."+x+"-"+y).addClass("cell-click");
 }
