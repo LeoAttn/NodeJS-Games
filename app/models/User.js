654eb238079e5e219686390ac6168372bf2759e2
Léo <leo.massonnat@hotmail.fr>
2015-12-15 11:54:46 +0100

    bcrypt

--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt-nodejs'),
+    SALT = 10;
 
 var schema = new Schema({
     first_name: {type: String, required: false, default: ''},
@@ -11,5 +13,28 @@ var schema = new Schema({
     wins :{type : Number, default : 0},
     createdOn: {type: Date, default: Date.now}
 });
+
+schema.pre('save', function(next) {
+    var user = this;
+
+    // si le mot de passe est modifier ou creer
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+schema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
 
 exports.model = mongoose.model('User', schema, 'Users');
